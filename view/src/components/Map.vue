462e51b1d843c073cb411d6e79858ff90052c7bd
monowebjp <capybara@monoweb.jp>
2021-08-17 17:43:07 +0900

    map 置き換え

--- view/src/components/Map.vue
+++ view/src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map">
     <div class="row" :style="{width: chipSize * horizontal + 'px' }" v-for="(innerDungeons, index) in entireDungeons" :key="index">
-      <div class="col wall" v-for="item in innerDungeons" :key="item.id">
+      <div :class="{ col: true, wall: item.chip === 9 }" v-for="item in innerDungeons" :key="item.id">
         {{item.chip}}
       </div>
     </div>
@@ -46,15 +46,52 @@ export default class Map extends Vue {
       }
     }
 
-    return dungeons
+    return this.generateWay(dungeons, startCoordinate);
   }
 
-  calcCoordinate () {
-    const coordinate = {}
-    coordinate.x = Math.floor(Math.random() * this.horizontal)
-    coordinate.y = Math.floor(Math.random() * this.vertical)
+  calcCoordinate (startCoordinate = false) {
+    const coordinate = {};
+
+    while (startCoordinate.x === coordinate.x && startCoordinate.y === coordinate.y) {
+      coordinate.x = Math.floor(Math.random() * (this.horizontal - 2) + 1);
+      coordinate.y = Math.floor(Math.random() * (this.vertical - 2) + 1);
+    }
 
     return coordinate
+  }
+
+  generateWay (dungeons, prevCoordinate) {
+    const way = {x: prevCoordinate.x, y: prevCoordinate.y};
+    let candidate = [];
+
+    while (dungeons[way.x][way.y].chip !== "G") {
+      // TODO:移動先の候補の条件を再考
+      candidate.push(prevCoordinate.x);
+      if (prevCoordinate.x - 1 !== 0) candidate.push(prevCoordinate.x - 1);
+      if (prevCoordinate.x + 1 !== this.horizontal - 1) candidate.push(prevCoordinate.x + 1);
+
+      way.x = candidate[Math.floor(Math.random() * candidate.length)];
+
+      candidate = [];
+
+      if(prevCoordinate.x === way.x) {
+        if (prevCoordinate.y - 1 !== 0) candidate.push(prevCoordinate.y - 1);
+        if (prevCoordinate.y + 1 !== this.vertical - 1) candidate.push(prevCoordinate.y + 1);
+
+        way.y = candidate[Math.floor(Math.random() * candidate.length)];
+      } else {
+        way.y = prevCoordinate.y;
+      }
+
+      // TODO:スタート地点、ゴール地点、すでに道になっているところは除く
+      if (dungeons[way.x][way.y].chip !== "G" && dungeons[way.x][way.y].chip !== "S") {
+        dungeons[way.x][way.y].chip = 'W';
+        prevCoordinate.x = way.x
+        prevCoordinate.y = way.y
+      }
+    }
+
+    return dungeons;
   }
 }
 </script>
@@ -72,6 +109,7 @@ export default class Map extends Vue {
   /*background-image: url(/static/img/base.png);*/
 }
 .wall {
-  background-position: -16px -112px;
+  //background-position: -16px -112px;
+  background-color: #000;
 }
 </style>
